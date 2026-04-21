93ff84ba1efaa83772d34fe2dbb6101866830eca
khushal1408 <98699996+khushal1408@users.noreply.github.com>
2026-04-21 18:09:23 +0530

    Bulk sync: 1 submission

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cs
@@ -0,0 +1,19 @@
+
+public class Solution {
+    public bool hasDuplicate(int[] nums) {
+        Dictionary<int,int> dict=new Dictionary<int,int>();
+
+        for(int i=0;i<nums.Length;i++)
+        {
+            if(dict.ContainsKey(nums[i]))
+            {
+                return true;
+            }
+            else
+            {
+                dict[nums[i]]=1;
+            }
+        }
+        return false;
+    }
+}
